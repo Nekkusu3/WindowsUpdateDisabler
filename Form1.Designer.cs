988a1db092f682feec3fcf904f54570d85e64f81
Nekkusu3 <noob0815noob@gmail.com>
2023-07-22 01:38:20 +0200

    1.0.01

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsUpdateDisabler
+{
+    partial class WindowsUpdateDisabler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WindowsUpdateDisabler));
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(304, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 85);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(600, 425);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(245, 59);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "Made By Nekkusu\nMore at: https://www.harambe.ch";
+            this.richTextBox1.Click += new System.EventHandler(this.OpenHarambe);
+            // 
+            // WindowsUpdateDisabler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsUpdateDisabler.Properties.Resources.IMG_274816;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(841, 481);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WindowsUpdateDisabler";
+            this.Text = "Windows Update Disabler";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace WindowsUpdateDisabler
+{
+    public partial class WindowsUpdateDisabler : Form
+    {
+        public WindowsUpdateDisabler()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer");
+            key.SetValue("NoWindowsUpdate", 1, RegistryValueKind.DWord);
+            key.Close();
+
+            RegistryKey key2 = Registry.LocalMachine.CreateSubKey(@"Software\Policies\Microsoft\Windows\WindowsUpdate\AU");
+            key2.SetValue("NoAutoUpdate", 1, RegistryValueKind.DWord);
+            key2.Close();
+
+            button1.Text = "Success!";
+
+        }
+
+
+        private void OpenHarambe(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.harambe.ch");
+        }
+    }
+}
